0255f47dfb7f64fb9c35d41d3a0ed3711c69b615
Rafael Trancoso <rafael.trancoso@ssp.to.gov.br>
2025-07-15 10:51:30 -0300

    inicio projeto

---
+++ primeirospassos/conversao_de_tipos.py
@@ -0,0 +1,22 @@
+
+print( 5 // 2)
+
+preco = 10
+print(preco)
+
+preco = float(preco)
+print(preco)
+
+preco = 8
+print(preco / 2)
+print(preco // 2)
+
+print(f'Preco e {preco}')
+
+valor = "20"
+print(int(valor))
+
+numero = 35
+print(str(numero))
+numero_texto = str(numero)
+print(type(numero_texto))
---
+++ primeirospassos/tipos_de_dados.py
@@ -0,0 +1,4 @@
+print(10 + 1)
+print(1.5 + 0.5)
+print(True)
+print('Meu Nome')
