359db7da21d85c9807b6cd6f01ddb98ece887c9f
Rafael Trancoso <rafaelptb@gmail.com>
2025-07-15 17:12:13 -0300

    operadores

---
+++ primeirospassos/comparacao.py
@@ -0,0 +1,9 @@
+valor1 = 300
+valor2 = 100
+
+print(valor1 == valor2)
+print(valor1 != valor2)
+print(valor1 > valor2)
+print(valor1 >= valor2)
+print(valor1 < valor2)
+print(valor1 <= valor2)
---
+++ primeirospassos/operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+# Adição
+print(1 + 1)
+
+# Subtração
+print(2 - 1)
+
+# Multiplicação
+print(2 * 3)
+
+# Divisão
+print(8 / 2)
+
+# Divisão Inteira
+print(15 // 3)
+
+# Exponenciação
+print(2 ** 2)
+
+# Módulo
+print(10 % 3)
